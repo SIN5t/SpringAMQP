70ced8c4ee64828fb73da02fbe2c3436bb095247
SIN5t <97775060+SIN5t@users.noreply.github.com>
2023-05-29 22:14:22 +0800

    springAMQP

--- publisher/src/main/java/cn/edu/uestc/PublisherApplication.java
+++ publisher/src/main/java/cn/edu/uestc/PublisherApplication.java
@@ -1,12 +1,18 @@
 package cn.edu.uestc;
 
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PublisherApplication {
     public static void main(String[] args) {
         SpringApplication.run(PublisherApplication.class, args);
+    }
+    @Bean
+    public org.springframework.amqp.support.converter.MessageConverter jsonMessageConverter(){
+        return new Jackson2JsonMessageConverter();
     }
 
 }
--- publisher/src/test/java/cn/edu/uestc/SpringAmqpTest.java
+++ publisher/src/test/java/cn/edu/uestc/SpringAmqpTest.java
@@ -8,6 +8,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.Map;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -26,6 +28,44 @@ public class SpringAmqpTest {
             Thread.sleep(20);
 
         }
+    }
+
+    @Test
+    public void testFanoutExchange(){
+        //交换机名称
+        String exchangeName = "uestc.fanout";
+        String msg = "hello fanoutMsg";
+        for (int i = 0; i < 1; i++) {
+            rabbitTemplate.convertAndSend(exchangeName,"",msg);
+        }
+    }
+
+    @Test
+    public void testDirectExchange(){
+        //交换机名称
+        String exchangeName = "directExchange";
+        String msg = "hello DirectMsg";
+        for (int i = 0; i < 1; i++) {
+            rabbitTemplate.convertAndSend(exchangeName,"white",msg);
+        }
+    }
+
+    @Test
+    public void testTopicExchange(){
+        //交换机名称
+        String exchangeName = "TopicExchange";
+        String msg = "hello TopicMsg";
+        for (int i = 0; i < 1; i++) {
+            rabbitTemplate.convertAndSend(exchangeName,"China.tec",msg);
+        }
+    }
+    @Test
+    public void testSendMap() throws InterruptedException {
+        Map<String,String> map = new HashMap<>();
+        map.put("name","张三");
+        map.put("age","21");
+        String queueName = "workQueue";
+        rabbitTemplate.convertAndSend(queueName,map);
 
     }
 
